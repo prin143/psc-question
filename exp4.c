b6d7f4befce6120cedc0ea16bd5fa6f3293bdb6c
Prince Kumar <princekumar9661176@gmail.com>
2025-12-15 12:24:20 +0530

    exp4.c

---
+++ exp4.c
@@ -0,0 +1,11 @@
+//Bitwise operators//
+
+#include <stdio.h>
+int main() {
+int a = 5, b = 3;
+printf("AND = %d\n", a & b);
+printf("OR = %d\n", a | b);
+printf("Left Shift = %d\n", a << 1);
+printf("Right Shift = %d\n", a >> 1);
+return 0;
+}
