c20f50302605222d7a6c593e7f0f6075a9fd41e2
Prince Kumar <princekumar9661176@gmail.com>
2025-12-15 12:13:34 +0530

    added all lab files

---
+++ exp1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+printf("Name: Prince Kumar\n");
+printf("Age: 18\n");
+printf("College: arka jain university \n");
+return 0;
+}
---
+++ exp2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define PI 3.14
+int main() {
+int a = 10;
+float b = 5.5;
+char c = 'A';
+printf("Int=%d Float=%.2f Char=%c PI=%.2f", a, b, c, PI);
+return 0;
+}
