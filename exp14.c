a5a51afc8a8f50672d2811f9071e741980708c9f
Prince Kumar <princekumar9661176@gmail.com>
2025-12-19 08:48:06 +0530

    Improve binary search with index output and prompts
    Enhanced binary search implementation to indicate the index of the found element and added user prompts for input.

--- exp14.c
+++ exp14.c
@@ -1,13 +1,31 @@
 #include <stdio.h>
+
 int main() {
-int a[10], l=0, h=9, m, key, i;
-for(i=0;i<10;i++) scanf("%d", &a[i]);
-scanf("%d", &key);
-while(l<=h) {
-m=(l+h)/2;
-if(a[m]==key) { printf("Found"); break; }
-else if(key<a[m]) h=m-1;
-else l=m+1;
-}
-return 0;
+    int a[10], l = 0, h = 9, m, key, i, found = 0;
+
+    printf("Enter 10 sorted elements:\n");
+    for(i = 0; i < 10; i++)
+        scanf("%d", &a[i]);
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    while(l <= h) {
+        m = (l + h) / 2;
+
+        if(a[m] == key) {
+            printf("Found at index %d", m);
+            found = 1;
+            break;
+        }
+        else if(key < a[m])
+            h = m - 1;
+        else
+            l = m + 1;
+    }
+
+    if(found == 0)
+        printf("Not Found");
+
+    return 0;
 }
